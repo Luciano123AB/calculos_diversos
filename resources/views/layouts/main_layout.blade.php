b75804bbc05a38db9bdc3da5d0442e7455c9bc87
Pedro Celso Alves da Silva <75758270059@senacrs.edu.br>
2025-10-02 19:28:42 -0300

    01(Home)

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -4,13 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Projeto</title>
-
+    <title>Projeto: Calculos Diversos</title>
     @include("layouts.partials.links")
 </head>
 <body class="text-center">
     @yield('content')
-
-    @include("layouts.partials.scripts")
 </body>
 </html>
