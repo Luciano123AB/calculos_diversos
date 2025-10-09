643fc6ca64e745bad8effef5c5f8efffa66c0a93
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-09 13:15:36 -0300

    003(Descontos_Cupons)

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,6 +1,11 @@
 <script>
-    function limparCampos() {
-        document.getElementById('valor').value = '';
-        document.getElementById('quantidade').value = '';
+    function limparCampos01() {
+        document.getElementById("valor").value = "";
+        document.getElementById("quantidade").value = "";
+    }
+
+    function limparCampos02() {
+        document.getElementById("valor").value = "";
+        document.getElementById("desconto").value = "";
     }
 </script>
