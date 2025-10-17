8a333f2b7874d99d6badb9a41b06827b8a2d9445
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-17 20:40:00 -0300

    011(Previsao_Ganhos_Perdas)

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -15,18 +15,50 @@
     }
 
     function limparCampos04() {
-        document.getElementById("valor_km").value = "";
-        document.getElementById("distancia").value = "";
+        document.getElementById("valor").value = "";
+        document.getElementById("taxa").value = "";
     }
 
     function limparCampos05() {
         document.getElementById("valor").value = "";
         document.getElementById("taxa").value = "";
+        document.getElementById("numero_meses").value = "";
     }
 
     function limparCampos06() {
         document.getElementById("valor").value = "";
+
+        var moeda = document.getElementById("moeda");
+
+        if (moeda) {
+
+            moeda.selectedIndex = 0;
+            
+        }
+    }
+
+    function limparCampos07() {
+        document.getElementById("valor").value = "";
+
+        var juros = document.getElementById("juros");
+
+        if (juros) {
+
+            juros.selectedIndex = 0;
+            
+        }
+
         document.getElementById("taxa").value = "";
-        document.getElementById("numero_meses").value = "";
+        document.getElementById("tempo").value = "";
+    }
+
+    function limparCampos08() {
+        document.getElementById("valor").value = "";
+        document.getElementById("taxa").value = "";
+    }
+
+    function limparCampos09() {
+        document.getElementById("receita").value = "";
+        document.getElementById("despesa").value = "";
     }
 </script>
