e42fd7308de73168a808241cb9e1801152cb13d6
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2025-10-24 19:45:43 -0300

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
         </div>
 
         <footer class="card-footer bg-info text-white text-center py-3">
-            <small>© {{ date("Y") }} Cálculos Diversos - Todos os direitos reservados.</small>
+            <img src="{{ asset("assets/images/foto_proprietario.png") }}" style="width: 35px; height: 35px;" class="border border-black shadow"><small> © {{ date("Y") }} Cálculos Diversos - Todos os direitos reservados: Luciano Eduardo Stefanello da Silva</small>
         </footer>
     </div>
 </div>
