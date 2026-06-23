ab1c873bf1bf150b8796204439759e24dd8d73dd
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-23 15:25:32 -0300

    026(Documentacao_Melhorias)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,5 +4,5 @@
 
 <footer class="bg-secondary border-top text-white text-center mt-auto py-3">
     <img src="{{ asset("assets/images/foto_proprietario.png") }}" style="width: 35px; height: 35px;" class="border border-black shadow rounded">
-    <small> © 2025 - {{ date("Y") }} {{ env("APP_NAME") }} / Todos os direitos reservados: Luciano Eduardo Stefanello da Silva</small>
+    <small> © 2025 - {{ date("Y") }} {{ config('app.name') }} / Todos os direitos reservados: Luciano Eduardo Stefanello da Silva</small>
 </footer>
